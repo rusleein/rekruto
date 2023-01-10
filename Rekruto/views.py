1e615987e83510ae7bb40e46b45a2bdd7e62b9de
Руслан Иванов <115787094+rusleein@users.noreply.github.com>
2023-01-10 16:17:20 +0300

    Update views.py

--- Rekruto/views.py
+++ Rekruto/views.py
@@ -4,4 +4,4 @@ from django.http import HttpResponse
 def index(request):
     name = request.GET.get("name")
     message = request.GET.get("message")
-    return HttpResponse(f"<h1>{name}!  {message}</h1> <title>Hello {name}!  {message}!</title>")
+    return HttpResponse(f"<h1>{name}!  {message}</h1> <title>{name}!  {message}!</title>")
